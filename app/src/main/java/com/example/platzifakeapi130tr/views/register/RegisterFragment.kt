309f11f18cc5da71d052c6f901179abda48c136c
SuRakib <rakibul15-10976@diu.edu.bd>
2023-07-26 10:37:12 +0600

    Register2 Commit

--- app/src/main/java/com/example/platzifakeapi130tr/views/register/RegisterFragment.kt
+++ app/src/main/java/com/example/platzifakeapi130tr/views/register/RegisterFragment.kt
@@ -1,17 +1,25 @@
 package com.example.platzifakeapi130tr.views.register
 
+import android.app.DownloadManager.Request
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.platzifakeapi130tr.R
 import com.example.platzifakeapi130tr.databinding.FragmentRegisterBinding
+import com.example.platzifakeapi130tr.models.register.RequestRegister
+import dagger.hilt.android.AndroidEntryPoint
 
-
+//S20
+@AndroidEntryPoint
 class RegisterFragment : Fragment() {
 
-    lateinit var binding: FragmentRegisterBinding
+    private  val viewModel : RegisterViewModel by viewModels()
+    private lateinit var binding: FragmentRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +28,54 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
        binding=FragmentRegisterBinding.inflate(inflater, container, false)
 
+        //add observer ..it works like ear
+        viewModel.registerResponse.observe(viewLifecycleOwner){
+
+
+            if (it.isSuccessful) {
+
+                Log.d("TAG", "DATA : ${it.body()} ")
+
+            }
+
+
+
+        }
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.registerBtn.setOnClickListener {
+
+            val name= binding.userName.text.toString().trim()
+            val email= binding.userEmail.text.toString().trim()
+            val password= binding.password.text.toString().trim()
+
+            handleRegister(name,email,password)
+
+
+        }
+        binding.loginBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+
+        }
+
+    }
+
+    private fun handleRegister(name: String, email: String, password: String) {
+
+        //call view model
+
+        val request= RequestRegister(name=name,
+            email = email,
+            password = password,
+            avatar = "https://img.freepik.com/free-vector/cute-cool-baby-holding-teddy-bear-doll-cartoon-vector-icon-illustration-people-holiday-isolated_138676-5356.jpg?w=2000")
+
+        viewModel.register(request)
     }
 
 
--- app/src/main/java/com/example/platzifakeapi130tr/views/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/platzifakeapi130tr/views/register/RegisterViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.platzifakeapi130tr.views.register
 
-class RegisterViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.platzifakeapi130tr.models.register.RequestRegister
+import com.example.platzifakeapi130tr.models.register.ResponseRegister
+import com.example.platzifakeapi130tr.repositories.AuthRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
+
+//S19
+@HiltViewModel
+class RegisterViewModel @Inject constructor(private val repo: AuthRepo) : ViewModel() {
+
+
+    private val _responseRegister = MutableLiveData<Response<ResponseRegister>>()
+    val registerResponse: LiveData<Response<ResponseRegister>> = _responseRegister
+
+    fun register(request: RequestRegister) {
+        viewModelScope.launch {
+            val data = repo.register(request)
+            _responseRegister.postValue(data)
+        }
+    }
+
 }
